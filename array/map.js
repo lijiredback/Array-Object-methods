60cc7d5d793742848b9a3e787a3cd21cc4cd4da4
liji <lijiredback@163.com>
2019-08-14 20:18:18 +0800

    feat:array.map

---
+++ array/map.js
@@ -0,0 +1,17 @@
+// map 方法返回一个函数，不改变原函数
+
+const originalArr = [1, 2, 3, 4, 5];
+
+function map(array, func) {
+    const newArr = [];
+    for (let i = 0; i < array.length; i++) {
+        newArr.push(func(array[i]));
+    }
+    return newArr;
+}
+
+const ret = map(originalArr, function(item) {
+    return item * 3;
+});
+
+console.log(ret);
